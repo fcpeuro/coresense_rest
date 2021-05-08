4521a9424eab551c459d1125978a63cf1e84efd5
Patrick McMorran <pat.mcmorran@fcpeuro.com>
2021-05-08 08:33:05 -0400

    added payload information to raise

--- lib/resources/Request_Write.rb
+++ lib/resources/Request_Write.rb
@@ -13,7 +13,7 @@ module CoresenseRest
 
     def create
       response = HTTParty.post(@root, body: @data.to_json, headers: @headers, format: :json, timeout: 120)
-      raise "#{response.parsed_response} code: #{response.code}" unless response.code >= 200 && response.code < 300
+      raise "#{response.parsed_response} code: #{response.code} \n payload: #{@data.to_json}" unless response.code >= 200 && response.code < 300
 
       @request_class.find(response.parsed_response['id'])
       # RequestRead.new(response.parsed_response['uri'], @headers, @request_class).select
