c2c62562bbca9114778d5f9c7d99c9d7c1bf0097
Marcus <marcus.cavini@fcpeuro.com>
2021-09-29 14:41:23 -0600

    Increase order creation

--- lib/resources/Request_Write.rb
+++ lib/resources/Request_Write.rb
@@ -12,7 +12,7 @@ module CoresenseRest
     end
 
     def create
-      response = HTTParty.post(@root, body: @data.to_json, headers: @headers, format: :json, timeout: 120)
+      response = HTTParty.post(@root, body: @data.to_json, headers: @headers, format: :json, timeout: 200)
       raise "#{response.parsed_response} code: #{response.code} \n payload: #{@data.to_json}" unless response.code >= 200 && response.code < 300
 
       @request_class.find(response.parsed_response['id'])
