ab748ebe25679610c44bcbcddfd7055649a72fc5
Brad Majewski <brad.majewski@fcpeuro.com>
2021-10-27 16:06:41 -0400

    Add ability to catch MultiJson errors.

--- lib/coresense_rest.rb
+++ lib/coresense_rest.rb
@@ -18,6 +18,26 @@ module CoresenseRest
   end
 
   class JSONParseError < HttpError
+    # Because some projects are using MultiJson which this project doesn't use.
+    # MultiJson overrides the ruby's json but throws different exceptions.
+    class Rescuable
+      CLASSES = ['MultiJson::ParseError', 'JSON::GeneratorError'].freeze
+
+      def self.===(exception)
+        exception.class.ancestors.any? { |klass| CLASSES.include?(klass.name) }
+      end
+    end
+
+    def self.from(exception, response)
+      msg = if exception.message == /line 1, column 1/
+        "line 1, column 1: #{response.body.to_s.slice(0..100)}"
+      else
+        exception.message
+      end
+
+      JSONParseError.new(msg, response)
+    end
+
     def original_exception
       cause
     end
